ef8b7b3eaaa1e43f1e6f4644803cd85c8ab9cc44
ANAND <anandchandran3306@gmail.com>
2024-06-28 12:33:30 +0530

    resume model completed.

--- src/components/view-resume/ResumeTemplate.jsx
+++ src/components/view-resume/ResumeTemplate.jsx
@@ -1,72 +1,93 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhone,
+  faEnvelope,
+  faMapMarkerAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 const ResumeTemplate = () => {
-    return (
-        <div className="max-w-screen-lg mx-auto bg-white shadow-lg p-8 my-8">
-            <div className="relative block page flex">
-                {/* Left Sidebar */}
-                <div className="w-1/3 bg-gray-200 py-10 px-6 relative">
-                    {/* Profile Image */}
-                    <div className="mugshot mb-4">
-                        <img src="/public/images/img-2.png" alt="Profile" className="w-full h-auto rounded-full" />
-                    </div>
-                    {/* Contact Information and Social Links */}
-                    <div>
-                        <p>123 My Place Drive</p>
-                        <p>Astoria, New York 11105</p>
-                        <p>1-800-CALLPLZ</p>
-                        <p>emailsareforsquares@gmail.com</p>
-                        <br/>
-                        <div>
-                            <h3 className="uppercase text-gray-600 mb-2 font-semibold">Contact Details</h3>
-                            <div className="flex items-center mb-2">
-                                <FontAwesomeIcon icon={faPhone} className="text-gray-700 mr-2" />
-                                <p>1-800-CALLPLZ</p>
-                            </div>
-                            <div className="flex items-center mb-2">
-                                <FontAwesomeIcon icon={faEnvelope} className="text-gray-700 mr-2" />
-                                <p>emailsareforsquares@gmail.com</p>
-                            </div>
-                            <div className="flex items-center">
-                                <FontAwesomeIcon icon={faMapMarkerAlt} className="text-gray-700 mr-2" />
-                                <p>123 My Place Drive, Astoria, New York 11105</p>
-                            </div>
-                        </div>
-                        <p className="uppercase block mt-6 font-semibold">Expertise</p>
-                        <p className="block list-thing">HTML</p>
-                        <p className="block list-thing">CSS (Stylus)</p>
-                        <p className="block list-thing">JavaScript & jQuery</p>
-                        <p className="block list-thing">Killer Taste</p>
-                        <p className="uppercase block mt-6 font-semibold">Education</p>
-                        <p className="block list-thing">Advanced potion making</p>
-                        <p className="block list-thing">Degree in popping and locking</p>
-                        <p className="block list-thing">Knitting game on point</p>
-                        <p className="block list-thing">Culinary af</p>
-                    </div>
-                </div>
-
-                {/* Main Content Container */}
-                <div className="relative content-container ml-6 px-10 py-6 w-2/3">
-                    <h2 className="uppercase text-center text-4xl font-semibold mb-4">Jr Front-End Developer</h2>
-                    <div className="block bg-gray-400 h-px w-96 mx-auto mb-6"></div>
-                    <div className="uppercase text-gray-600 mb-2">Profile</div>
-                    <p className="text-justify mb-8">Retro DIY quinoa, mixtape williamsburg master cleanse bushwick tumblr chillwave dreamcatcher hella wolf paleo...</p>
-                    <div className="uppercase text-gray-600 mb-2">Experience</div>
-                    <h3 className="font-semibold text-xl mb-2">Job #1</h3>
-                    <p className="text-gray-700 mb-4">First job description</p>
-                    <p className="text-justify mb-8">Plaid gentrify put a bird on it, pickled XOXO farm-to-table irony raw denim messenger bag leggings...</p>
-                    <h3 className="font-semibold text-xl mb-2">Job #2</h3>
-                    <p className="text-gray-700 mb-4">Second Job Description</p>
-                    <p className="text-justify mb-8">Beard before they sold out photo booth distillery health goth...</p>
-                    <h3 className="font-semibold text-xl mb-2">Job #3</h3>
-                    <p className="text-gray-700 mb-4">Third Job Description</p>
-                    <p className="text-justify mb-8">Next level roof party lo-fi fingerstache skateboard...</p>
-                </div>
+  return (
+    <div className="max-w-screen-lg mx-auto bg-white shadow-lg p-8 my-8">
+      <div className="relative block page flex">
+        {/* Left Sidebar */}
+        <div className="w-1/3 bg-gray-200 py-10 px-6 relative">
+          {/* Profile Image */}
+          <div className="mugshot mb-4">
+            <img
+              src="/public/images/img-2.png"
+              alt="Profile"
+              className="w-full h-auto rounded-full"
+            />
+          </div>
+          {/* Contact Information and Social Links */}
+          <div>
+            <br />
+            <div>
+              <h3 className="uppercase mb-2 font-semibold">Contact Details</h3>
+              <div className="flex items-center mb-2">
+                <FontAwesomeIcon
+                  icon={faPhone}
+                  className="text-gray-700 mr-2"
+                />
+                <p>1-800-CALLPLZ</p>
+              </div>
+              <div className="flex items-center mb-2">
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  className="text-gray-700 mr-2"
+                />
+                <p>emailsareforsquares@gmail.com</p>
+              </div>
+              <div className="flex items-center">
+                <FontAwesomeIcon
+                  icon={faMapMarkerAlt}
+                  className="text-gray-700 mr-2"
+                />
+                <p>123 My Place Drive, Astoria, New York 11105</p>
+              </div>
             </div>
+            <p className="uppercase block mt-12 font-semibold">Skills</p>
+            <p className="block list-thing">HTML</p>
+            <p className="block list-thing">CSS (Stylus)</p>
+            <p className="block list-thing">JavaScript & jQuery</p>
+            <p className="block list-thing">Killer Taste</p>
+            {/* <p className="uppercase block mt-6 font-semibold">Education</p>
+            <p className="block list-thing">Advanced potion making</p>
+            <p className="block list-thing">Degree in popping and locking</p>
+            <p className="block list-thing">Knitting game on point</p>
+            <p className="block list-thing">Culinary af</p> */}
+          </div>
         </div>
-    );
-}
+
+        {/* Main Content Container */}
+        <div className="relative content-container ml-6 px-10 py-6 w-2/3">
+          <h2 className="uppercase text-center text-4xl font-semibold mb-4">
+            John Doe
+          </h2>
+          <div className="block bg-gray-400 h-px w-96 mx-auto mb-6"></div>
+
+          <div className="uppercase text-gray-600  mb-2">
+            Educational Details
+          </div>
+          <h3 className="font-medium text-lg mb-2">Highest Qualification</h3>
+          <p className="text-gray-700 mb-4">qualifictaion detaails here</p>
+          <h3 className="font-medium text-lg mb-2">Institute</h3>
+          <p className="text-gray-700 mb-4">institute name</p>
+          <h3 className="font-medium text-lg mb-2">Board</h3>
+          <p className="text-gray-700 mb-4">board</p>
+          <h3 className="font-medium text-lg mb-2">Additional Qualification</h3>
+          <p className="text-gray-700 mb-4">addional qualifications here</p>
+
+          <div className="uppercase text-gray-600 mt-12 mb-2">Experience</div>
+          <h3 className="font-medium text-lg mb-2">Years of experience</h3>
+          <p className="text-gray-700 mb-4">no of years</p>
+          <h3 className="font-medium text-lg mb-2">Expected Salary</h3>
+          <p className="text-gray-700 mb-4">expected salary</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ResumeTemplate;
